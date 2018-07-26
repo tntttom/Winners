06c848c01252ddca454981f6db138fb74dbedd09
tommynguyen <tnguyen21@luc.edu>
2018-07-26 09:50:07 -0500

    Initial Commit

---
+++ Winners/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Winners
+//
+//  Created by Tommy Nguyen on 7/26/18.
+//  Copyright © 2018 Tommy Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
